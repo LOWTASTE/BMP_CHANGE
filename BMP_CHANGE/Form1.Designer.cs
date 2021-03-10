e3369e71ca518fb122ae3346cc7976ee852e7844
LUNTAO LIU <544851589@qq.com>
2021-03-10 19:23:49 +0800

    添加项目文件。

---
+++ BMP_CHANGE/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace BMP_CHANGE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_f = new System.Windows.Forms.Button();
+            this.btn_e = new System.Windows.Forms.Button();
+            this.btn_r = new System.Windows.Forms.Button();
+            this.pictureBox_r = new System.Windows.Forms.PictureBox();
+            this.pictureBox_f = new System.Windows.Forms.PictureBox();
+            this.pictureBox_e = new System.Windows.Forms.PictureBox();
+            this.textBox_c = new System.Windows.Forms.TextBox();
+            this.button_exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_r)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_f)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_e)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_f
+            // 
+            this.btn_f.Location = new System.Drawing.Point(12, 335);
+            this.btn_f.Name = "btn_f";
+            this.btn_f.Size = new System.Drawing.Size(150, 50);
+            this.btn_f.TabIndex = 0;
+            this.btn_f.Text = "起始幀";
+            this.btn_f.UseVisualStyleBackColor = true;
+            this.btn_f.Click += new System.EventHandler(this.btn_f_Click);
+            // 
+            // btn_e
+            // 
+            this.btn_e.Location = new System.Drawing.Point(322, 335);
+            this.btn_e.Name = "btn_e";
+            this.btn_e.Size = new System.Drawing.Size(150, 50);
+            this.btn_e.TabIndex = 1;
+            this.btn_e.Text = "截止幀";
+            this.btn_e.UseVisualStyleBackColor = true;
+            this.btn_e.Click += new System.EventHandler(this.btn_e_Click);
+            // 
+            // btn_r
+            // 
+            this.btn_r.Location = new System.Drawing.Point(168, 335);
+            this.btn_r.Name = "btn_r";
+            this.btn_r.Size = new System.Drawing.Size(150, 50);
+            this.btn_r.TabIndex = 2;
+            this.btn_r.Text = "運行";
+            this.btn_r.UseVisualStyleBackColor = true;
+            this.btn_r.Click += new System.EventHandler(this.btn_r_Click);
+            // 
+            // pictureBox_r
+            // 
+            this.pictureBox_r.Location = new System.Drawing.Point(168, 179);
+            this.pictureBox_r.Name = "pictureBox_r";
+            this.pictureBox_r.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox_r.TabIndex = 3;
+            this.pictureBox_r.TabStop = false;
+            // 
+            // pictureBox_f
+            // 
+            this.pictureBox_f.Location = new System.Drawing.Point(12, 179);
+            this.pictureBox_f.Name = "pictureBox_f";
+            this.pictureBox_f.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox_f.TabIndex = 4;
+            this.pictureBox_f.TabStop = false;
+            // 
+            // pictureBox_e
+            // 
+            this.pictureBox_e.Location = new System.Drawing.Point(322, 179);
+            this.pictureBox_e.Name = "pictureBox_e";
+            this.pictureBox_e.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox_e.TabIndex = 5;
+            this.pictureBox_e.TabStop = false;
+            // 
+            // textBox_c
+            // 
+            this.textBox_c.Location = new System.Drawing.Point(12, 12);
+            this.textBox_c.Multiline = true;
+            this.textBox_c.Name = "textBox_c";
+            this.textBox_c.ReadOnly = true;
+            this.textBox_c.Size = new System.Drawing.Size(460, 150);
+            this.textBox_c.TabIndex = 6;
+            // 
+            // button_exit
+            // 
+            this.button_exit.BackColor = System.Drawing.Color.Firebrick;
+            this.button_exit.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_exit.Location = new System.Drawing.Point(204, 410);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(75, 53);
+            this.button_exit.TabIndex = 7;
+            this.button_exit.Text = "退出";
+            this.button_exit.UseVisualStyleBackColor = false;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 475);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.textBox_c);
+            this.Controls.Add(this.pictureBox_e);
+            this.Controls.Add(this.pictureBox_f);
+            this.Controls.Add(this.pictureBox_r);
+            this.Controls.Add(this.btn_r);
+            this.Controls.Add(this.btn_e);
+            this.Controls.Add(this.btn_f);
+            this.Name = "Form1";
+            this.Text = "BMPCHANGE";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_r)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_f)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_e)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_f;
+        private System.Windows.Forms.Button btn_e;
+        private System.Windows.Forms.Button btn_r;
+        private System.Windows.Forms.PictureBox pictureBox_r;
+        private System.Windows.Forms.PictureBox pictureBox_f;
+        private System.Windows.Forms.PictureBox pictureBox_e;
+        private System.Windows.Forms.TextBox textBox_c;
+        private System.Windows.Forms.Button button_exit;
+    }
+}
+
---
+++ BMP_CHANGE/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMP_CHANGE
+{
+    public partial class Form1 : Form
+    {
+        public static Bitmap BMP_F;   
+        public static Bitmap BMP_E;
+        public static Bitmap BMP_R;
+        static  System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        private static bool exitFlag = false;  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_f_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string file_f = chooseBmp();
+                BMP_F = new Bitmap(file_f);
+                pictureBox_f.Image = BMP_F;
+                int BMPWidth = BMP_F.Width;
+                int BMPHeight = BMP_F.Height;
+                textBox_c.Text = "起始帧:\n" + file_f + "\n宽度为:" + BMP_F.Width +  " \n高度为:" + BMP_F.Height;
+
+            }
+            catch
+            {
+                textBox_c.Text = "请选择图片";
+            }
+        }
+
+        private void btn_e_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string file_e = chooseBmp();
+                BMP_E = new Bitmap(file_e);
+                pictureBox_e.Image = BMP_E;
+                textBox_c.Text = file_e;
+                textBox_c.Text = "截至帧:\n" + file_e + "\n宽度为:" + BMP_E.Width + " \n高度为:" + BMP_E.Height;
+            }
+            catch
+            {
+                textBox_c.Text = "请选择图片";
+            }
+        }
+
+        private void btn_r_Click(object sender, EventArgs e)
+        {
+
+            if(BMP_F==null||BMP_E==null)
+            {
+                btn_f_Click(sender, e);
+                btn_e_Click(sender, e);
+            }
+            if(BMP_E.Width!=BMP_F.Width||BMP_E.Height!=BMP_E.Height)
+            {
+                textBox_c.Text = "请重新选择大小相等的图片";
+                btn_f_Click(sender, e);
+                btn_e_Click(sender, e);
+            }
+
+            BMP_R = (Bitmap)BMP_F.Clone();
+
+            Timer_Load(sender, e);
+            while (exitFlag == false)
+            {
+                // Processes all the events in the queue.
+                Application.DoEvents();
+                pictureBox_r.Image = BMP_R;
+            }
+            if(exitFlag == true)
+            {
+                myTimer.Stop();
+            }
+
+        }
+
+        public string chooseBmp()
+        {
+            string bmpfile = "";
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择位图文件";
+            dialog.Filter = "图片文件(*.bmp)|*.bmp";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bmpfile = dialog.FileName;
+            }
+            return bmpfile;
+        }
+
+
+        public static Color changeRGB(Color des,Color src)
+        {
+
+            if (des.A > src.A)
+            {
+                src = Color.FromArgb(src.A + 1, src.R, src.G, src.B);
+            }
+            if (des.A < src.A)
+            {
+                src = Color.FromArgb(src.A - 1, src.R, src.G, src.B);
+            }
+
+
+            if (des.R > src.R)
+            {
+                src = Color.FromArgb(src.R + 1,src.G,src.B);
+            }
+            if(des.R < src.R)
+            {
+                src = Color.FromArgb(src.R - 1, src.G, src.B);
+            }
+
+            if (des.G > src.G)
+            {
+                src = Color.FromArgb(src.R, src.G + 1, src.B);
+            }
+            if (des.G < src.G)
+            {
+                src = Color.FromArgb(src.R, src.G - 1, src.B);
+            }
+
+            if (des.B > src.B)
+            {
+                src = Color.FromArgb(src.R, src.G, src.B + 1);
+            }
+            if (des.B < src.B)
+            {
+                src = Color.FromArgb(src.R, src.G, src.B - 1);
+            }
+            return src;
+        }
+
+        private void Timer_Load(object sender, EventArgs e)
+        {
+            // Sets the timer interval to 1 seconds.
+            // myTimer.Interval = 1000;
+            myTimer.Interval = 10;
+
+            /* Adds the event and the event handler for the method that will 
+                                    process the timer event to the timer. */
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+            myTimer.Start();
+
+            // Runs the timer, and raises the event.
+            //this._TimersTimer = new System.Timers.Timer();
+            //this._TimersTimer.Interval = 100;
+            //this._TimersTimer.Elapsed += new System.Timers.ElapsedEventHandler(_TimersTimer_Elapsed);
+            //this._TimersTimer.Start();
+        }
+        private static void TimerEventProcessor(Object myObject, EventArgs myEventArgs)
+        {
+            myTimer.Stop();
+            //Color pixelColor = Color.FromName("Red");
+            int BMPWidth = BMP_F.Width;
+            int BMPHeight = BMP_F.Height;
+
+            for (int i = 0; i < BMPWidth; i++)
+            {
+                for (int j = 0; j < BMPHeight; j++)
+                {
+                    Color Dcolor = BMP_E.GetPixel(i, j);
+                    Color Rcolor = BMP_R.GetPixel(i, j);
+                    Color RES = changeRGB(Dcolor,Rcolor);
+                    BMP_R.SetPixel(i, j, RES);
+                }
+            }
+
+
+            if (BMP_R.Equals(BMP_E))
+            {
+                myTimer.Enabled = false;
+                exitFlag = true;
+            }
+            else
+            {
+                myTimer.Enabled = true;
+            }
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
